2f0b83b2f883581b944f03472204124f4aa90ab7
abolfazl mashhadi <abolfazlmashhadi93@gmail.com>
2024-10-28 20:18:51 -0700

    created to control socket event names to provide constant names

---
+++ app/socket/event-names.js
@@ -0,0 +1,8 @@
+const SocketEventNames=Object.freeze({
+    IN_APP_MESSAGE:"IN_APP_MESSAGE",
+    SERVER_ERROR:"SERVER_ERROR",
+    SERVER_MASSAGE:"SERVER_MASSAGE",
+})
+
+
+module.exports = SocketEventNames
