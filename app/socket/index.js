5d353be8467fd916fff5ff42d70dec9705e24261
abolfazl mashhadi <abolfazlmashhadi93@gmail.com>
2024-10-30 19:39:41 -0700

    added quizGame name space in socket and handled middleware

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,26 +1,34 @@
-const { addPlayerToQueue, removePlayerFromQueue } = require("../redis/redis_cache_service");
-const { startGame, gameSocketHandler} = require("./handlers/startGame.handler");
-const socketAuthMiddle = require("./middleware/socket.auth.middleware");
+const {addPlayerToQueue, removePlayerFromQueue} = require("../redis/redis_cache_service");
+const {startGame, gameSocketHandler} = require("./handlers/startGame.handler");
+
+
+const {socketAuthMiddle, gameAuthMiddleWare} = require("./middleware/socket.auth.middleware");
 
 const ioMainHandler = (io) => {
-  try{
-    let mainAppOnlineSpace=io.of('/online')
-    mainAppOnlineSpace.use(socketAuthMiddle)
-    mainAppOnlineSpace.on('connection',(socket)=>{
-        let user=socket.user
-        user.socketId=socket.id
-        console.log(`an authorized user connected with email of ${user.email}`)
-        gameSocketHandler(io,socket,user)
-        socket.on('disconnect',(_)=>{
-          removePlayerFromQueue(user.email,user.level)
+    try {
+        let mainAppOnlineSpace = io.of('/online')
+
+        mainAppOnlineSpace.use(socketAuthMiddle)
+        mainAppOnlineSpace.on('connection', (socket) => {
+            let user = socket.user
+            user.socketId = socket.id
+            console.log(`an authorized user connected with email of ${user.email}`)
+            gameSocketHandler(io, socket, user)
+            socket.on('disconnect', (_) => {
+                removePlayerFromQueue(user.email, user.level)
+            })
         })
-    })
-    
-  }catch(e){
-    console.log(e)
-  }
+        let quizGameSpace = io.of('/quizGame')
+        quizGameSpace.use(gameAuthMiddleWare)
+        quizGameSpace.on('connection', (socket) => {
+            console.log('a player joined to private rooms of quiz game ')
+        })
+
+    } catch (e) {
+        console.log(e)
+    }
 };
 
 module.exports = {
-  ioMainHandler,
+    ioMainHandler,
 };
