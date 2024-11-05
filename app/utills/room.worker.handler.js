c900aec7e40ddb4d349a0110ec7d8b7f42291e73
abolfazl mashhadi <abolfazlmashhadi93@gmail.com>
2024-11-04 21:22:45 -0800

    commented the log in file

--- app/utills/room.worker.handler.js
+++ app/utills/room.worker.handler.js
@@ -3,6 +3,7 @@ const {workerData,parentPort}=require('worker_threads')
 const {QuizGame} = require("./quizGame");
 
 
-console.log(workerData);
-let quizGame=new QuizGame()
-quizGame.startGame(workerData.gameId)
+// console.log(workerData);
+let {questionPerRoom,quizTimeLimit}=workerData;
+let quizGame=new QuizGame(parentPort)
+quizGame.startGame(questionPerRoom,quizTimeLimit);
